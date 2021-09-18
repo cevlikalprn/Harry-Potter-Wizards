373f81ec443608cdf5b71ed793e0b9ad10f18c3b
cevlikalprn <devcalprn@gmail.com>
2021-09-18 10:05:36 +0300

    Harry Potter Service class added

--- app/src/main/java/com/cevlikalprn/harrypotterwizards/network/HarryPotterService.kt
+++ app/src/main/java/com/cevlikalprn/harrypotterwizards/network/HarryPotterService.kt
@@ -1,12 +1,17 @@
 package com.cevlikalprn.harrypotterwizards.network
 
+import com.cevlikalprn.harrypotterwizards.models.Wizard
+import com.cevlikalprn.harrypotterwizards.models.WizardItem
 import com.cevlikalprn.harrypotterwizards.util.Constants.BASE_URL
+import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.GET
 
 interface HarryPotterService {
 
-
+    @GET("api/characters")
+    fun getWizards(): Response<Wizard>
 
 }
 
