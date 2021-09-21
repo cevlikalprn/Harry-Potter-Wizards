44643d0f0f5d1e6cd1657ada622712d2a193ac63
cevlikalprn <devcalprn@gmail.com>
2021-09-21 12:06:07 +0300

    Data passed to the recyclerView adapter

--- app/src/main/java/com/cevlikalprn/harrypotterwizards/ui/fragments/WizardListFragment.kt
+++ app/src/main/java/com/cevlikalprn/harrypotterwizards/ui/fragments/WizardListFragment.kt
@@ -5,16 +5,45 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProvider
 import com.cevlikalprn.harrypotterwizards.R
+import com.cevlikalprn.harrypotterwizards.adapter.WizardAdapter
+import com.cevlikalprn.harrypotterwizards.databinding.FragmentWizardsBinding
+import com.cevlikalprn.harrypotterwizards.network.HarryPotterApi
+import com.cevlikalprn.harrypotterwizards.repository.WizardRepository
+import com.cevlikalprn.harrypotterwizards.viewmodel.WizardListViewModel
+import com.cevlikalprn.harrypotterwizards.viewmodel.WizardListViewModelFactory
 
 class WizardListFragment : Fragment() {
+
+    private lateinit var binding: FragmentWizardsBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_wizards, container, false)
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_wizards, container, false)
+        return binding.root
+    }
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val repository = WizardRepository()
+        val viewModelFactory = WizardListViewModelFactory(repository)
+        val viewModel =
+            ViewModelProvider(this, viewModelFactory).get(WizardListViewModel::class.java)
+
+        val adapter = WizardAdapter()
+        binding.wizardListRecyclerView.adapter = adapter
+
+        viewModel.wizards.observe(viewLifecycleOwner) { wizards ->
+            adapter.data = wizards
+        }
+
+
     }
 
 }
