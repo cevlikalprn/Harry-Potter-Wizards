c968494c59d22b905c5d38a51591d9164e152354
cevlikalprn <devcalprn@gmail.com>
2021-09-27 19:26:19 +0300

    BindingAdapters.kt and strings.xml updated

--- app/src/main/java/com/cevlikalprn/harrypotterwizards/util/BindingAdapters.kt
+++ app/src/main/java/com/cevlikalprn/harrypotterwizards/util/BindingAdapters.kt
@@ -4,33 +4,31 @@ import android.view.View
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.databinding.BindingAdapter
+import com.cevlikalprn.harrypotterwizards.R
 import com.squareup.picasso.Picasso
 
 @BindingAdapter("app:isTheWizardAlive")
-fun isTheWizardAlive(textView: TextView, state: Boolean){
-    textView.text = if (state) "Alive" else "Not Alive"
+fun isTheWizardAlive(textView: TextView, state: Boolean) {
+    textView.text =
+        if (state) textView.context.getString(R.string.alive) else textView.context.getString(R.string.not_alive)
 }
 
 @BindingAdapter("app:setTheWizardImageUrl")
-fun setTheWizardImageUrl(imageView: ImageView, str: String){
+fun setTheWizardImageUrl(imageView: ImageView, str: String) {
     Picasso.get().load(str).into(imageView)
 }
 
 @BindingAdapter("app:showErrorMessageText")
-fun showErrorMessageText(textView: TextView, errorMessage: String?){
-    if (errorMessage != null) {
-        if(errorMessage.isNotEmpty()){
-            textView.text = errorMessage
-            textView.visibility = View.VISIBLE
-        }
+fun showErrorMessageText(textView: TextView, errorMessage: String?) {
+    if (!errorMessage.isNullOrEmpty()) {
+        textView.text = errorMessage
+        textView.visibility = View.VISIBLE
     }
 }
 
 @BindingAdapter("app:showErrorMessageIcon")
-fun showErrorMessageIcon(imageView: ImageView, errorMessage: String?){
-    if (errorMessage != null) {
-        if (errorMessage.isNotEmpty()){
-            imageView.visibility = View.VISIBLE
-        }
+fun showErrorMessageIcon(imageView: ImageView, errorMessage: String?) {
+    if (!errorMessage.isNullOrEmpty()) {
+        imageView.visibility = View.VISIBLE
     }
 }
