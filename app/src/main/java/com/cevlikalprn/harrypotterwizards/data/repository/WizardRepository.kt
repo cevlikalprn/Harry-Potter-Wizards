af7d6915a2cb931bf8d2a86068d06b0ad02375fb
cevlikalprn <devcalprn@gmail.com>
2021-10-03 00:45:40 +0300

    WizardRepository.kt updated

--- app/src/main/java/com/cevlikalprn/harrypotterwizards/data/repository/WizardRepository.kt
+++ app/src/main/java/com/cevlikalprn/harrypotterwizards/data/repository/WizardRepository.kt
@@ -1,10 +1,13 @@
 package com.cevlikalprn.harrypotterwizards.data.repository
 
+import com.cevlikalprn.harrypotterwizards.data.LocalDataSource
 import com.cevlikalprn.harrypotterwizards.data.RemoteDataSource
 
 class WizardRepository(
-    remoteDataSource: RemoteDataSource
+    remoteDataSource: RemoteDataSource,
+    localDataSource: LocalDataSource
 ) {
 
     val remote = remoteDataSource
+    val local = localDataSource
 }
