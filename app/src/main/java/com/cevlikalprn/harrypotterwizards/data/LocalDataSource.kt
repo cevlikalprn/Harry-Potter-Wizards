3447bca654910a85b60368ca27f3088fdec0c251
cevlikalprn <devcalprn@gmail.com>
2021-10-03 00:44:54 +0300

    LocalDataSource.kt added

---
+++ app/src/main/java/com/cevlikalprn/harrypotterwizards/data/LocalDataSource.kt
@@ -0,0 +1,8 @@
+package com.cevlikalprn.harrypotterwizards.data
+
+import com.cevlikalprn.harrypotterwizards.data.database.WizardDao
+
+class LocalDataSource(private val wizardDao: WizardDao) {
+
+
+}
