0ab094c37d950b8e7ff8d09104071eb35b176aac
cevlikalprn <devcalprn@gmail.com>
2021-10-04 11:37:34 +0300

    LocalDataSource.kt updated

--- app/src/main/java/com/cevlikalprn/harrypotterwizards/data/LocalDataSource.kt
+++ app/src/main/java/com/cevlikalprn/harrypotterwizards/data/LocalDataSource.kt
@@ -1,8 +1,12 @@
 package com.cevlikalprn.harrypotterwizards.data
 
+import androidx.lifecycle.LiveData
 import com.cevlikalprn.harrypotterwizards.data.database.WizardDao
+import com.cevlikalprn.harrypotterwizards.data.database.WizardEntity
 
-class LocalDataSource(private val wizardDao: WizardDao) {
+class LocalDataSource(wizardDao: WizardDao) {
 
+    val allWizards: LiveData<List<WizardEntity>> = wizardDao.getAllWizards()
+    val favoriteWizards: LiveData<List<WizardEntity>> = wizardDao.getFavoriteWizards()
 
 }
