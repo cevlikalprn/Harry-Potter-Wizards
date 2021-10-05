ce336a04a166d44d180521c2bafd53e823a57941
cevlikalprn <devcalprn@gmail.com>
2021-10-05 23:26:59 +0300

    FavoriteWizards fragment added

---
+++ app/src/main/java/com/cevlikalprn/harrypotterwizards/favorite/FavoriteWizardsFragment.kt
@@ -0,0 +1,37 @@
+package com.cevlikalprn.harrypotterwizards.favorite
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProvider
+import com.cevlikalprn.harrypotterwizards.R
+import com.cevlikalprn.harrypotterwizards.databinding.FragmentFavoriteWizardsBinding
+import com.cevlikalprn.harrypotterwizards.di.MyApplication
+
+
+class FavoriteWizardsFragment : Fragment() {
+
+    private lateinit var binding: FragmentFavoriteWizardsBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding =
+            DataBindingUtil.inflate(inflater, R.layout.fragment_favorite_wizards, container, false)
+        return inflater.inflate(R.layout.fragment_favorite_wizards, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val appContainer = (requireActivity().application as MyApplication).appContainer
+
+        val viewModel = ViewModelProvider(this, appContainer.favoriteWizardsViewModel).get(
+            FavoriteWizardsViewModel::class.java
+        )
+    }
+}
